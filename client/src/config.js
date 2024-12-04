1823d119632af64f94631750990959f14d7c1998
Sathya Venugopal <sathya@joinvulse.com>
2024-12-03 23:49:46 -0800

    update url

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,3 @@
 const inProduction = process.env.NODE_ENV === 'production';
 
-export const API_URL = inProduction ? 'https://sba-n63p.onrender.com:5001/api' : 'http://localhost:5001/api';
+export const API_URL = inProduction ? 'https://sba-n63p.onrender.com/api' : 'http://localhost:5001/api';
